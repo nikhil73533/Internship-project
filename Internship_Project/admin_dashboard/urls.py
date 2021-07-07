8ed73630d9edef951da4da070120ab04bb5fc80b
Harshverrdhon <harshtanwar@jklu.edu.in>
2021-07-08 03:42:32 +0530

    Added email verification functionality

--- Internship_Project/admin_dashboard/urls.py
+++ Internship_Project/admin_dashboard/urls.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
 from django.urls import path
+
 #<==== Importing function from views of admin dashboard =======>
-from .views import Login,Register
-#<-======= end of code===>
+from .views import Login, Register, Verification, Login_View
+
 #<===== Reset Password========>
 from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView,PasswordResetConfirmView,PasswordResetCompleteView
-#<===============end of code ========>
 
 urlpatterns = [
+
+    path('activate/<uidb64>/<token>', Verification.as_view(), name = "activate"),
+    path('login', Login_View.as_view(), name = "login"),
     #<=========login page Urls============>
     path('', Login, name = "Login"),
-    #<=======end of code ==============>
 
     #<=========Register page Urls============>
     path('Register', Register, name = "Register"),
-    #<=======end of code ==============>
    
     #<=========Password Reset Urls============>
     path('reset_password/',PasswordResetView.as_view(template_name = "reset_password/forgot_pass.html"),name = 'reset_password'),
     path('reset_password_sent/',PasswordResetDoneView.as_view(template_name = "reset_password/alert_message.html"), name = "password_reset_done"),
     path('reset/<uidb64>/<token>', PasswordResetConfirmView.as_view(template_name = "reset_password/new_pass.html"), name = "password_reset_confirm"),
     path('reset_password_complete/',PasswordResetCompleteView.as_view(template_name = "reset_password/pass_confirm.html"), name = "password_reset_complete"),
-   # <============ END OF CODE===============================>
 
 ]
 
